34818bd13d185f986445a82efbe8a5b0af40fe0c
Chris Trahey <christarhey@gmail.com>
2022-02-04 12:40:00 -0800

    update confirm.py to run trivial program on GPU

--- confirm.py
+++ confirm.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python
 import tensorflow as tf
+tf.debugging.set_log_device_placement(True)
 
 print(f"Running TensorFlow {tf.__version__} with {len(tf.config.list_physical_devices('GPU'))} GPUs recognized")
+
+with tf.device('/gpu:0'):
+  d = tf.random_normal([])
+  print(d)
+
